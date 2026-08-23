e63430cd226c5e4709a30fa3a28207f5efd9a16f
382868331 <382868331@users.noreply.github.com>
2026-08-24 06:07:05 +0800

    challenge: undercount string geohash precision

---
+++ task_009_test.go
@@ -0,0 +1,5 @@
+package geohash
+import "testing"
+func TestTask009StringPrecisionBits(t *testing.T){
+	for _,h:=range[]string{"u","u4","u4pruy"}{_,bits:=ConvertStringToInt(h);if bits!=uint(5*len(h)){t.Fatalf("%q bits=%d",h,bits)}}
+}
