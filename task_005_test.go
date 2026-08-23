96f5f15d306cd26057d457a9e30c0708f66975cc
382868331 <382868331@users.noreply.github.com>
2026-08-24 05:59:48 +0800

    challenge: skip first geohash byte validation

---
+++ task_005_test.go
@@ -0,0 +1,6 @@
+package geohash
+import "testing"
+func TestTask005ValidateChecksFirstByte(t *testing.T){
+	for _,h:=range []string{"a123","i9","o"}{if err:=Validate(h);err==nil{t.Fatalf("invalid hash %q accepted",h)}}
+	if err:=Validate("u4pr");err!=nil{t.Fatal(err)}
+}
