b4c2da753ed8176a3a0f7093670f6870cb9e3020
382868331 <382868331@users.noreply.github.com>
2026-08-24 06:21:55 +0800

    challenge: misalign integer bounding boxes

---
+++ task_017_test.go
@@ -0,0 +1,5 @@
+package geohash
+import "testing"
+func TestTask017IntegerBoxContainsEncodedPoint(t *testing.T){
+	for _,p:=range[][2]float64{{57.64911,10.40744},{-33.86,151.21}}{h:=EncodeIntWithPrecision(p[0],p[1],30);b:=BoundingBoxIntWithPrecision(h,30);if !b.Contains(p[0],p[1]){t.Fatalf("point=%v box=%#v",p,b)}}
+}
