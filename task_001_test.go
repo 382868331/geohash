5c9517744dd5d3e1e5154312414447f5161dd165
382868331 <382868331@users.noreply.github.com>
2026-08-24 05:54:53 +0800

    fix: recognize geohash alphabet bytes

--- task_001_test.go
+++ task_001_test.go
@@ -1,6 +1,19 @@
 package geohash
+
 import "testing"
-func TestTask001EncodingValidity(t *testing.T){
-	for _,b:=range []byte{'0','b','z'}{if !base32encoding.ValidByte(b){t.Fatalf("%q should be valid",b)}}
-	for _,b:=range []byte{'a','i','o'}{if base32encoding.ValidByte(b){t.Fatalf("%q should be invalid",b)}}
+
+func TestTask001EncodingValidity(t *testing.T) {
+	for _, b := range []byte{'0', 'b', 'z'} {
+		if !base32encoding.ValidByte(b) {
+			t.Fatalf("%q should be valid", b)
+		}
+	}
+	for _, b := range []byte{'a', 'i', 'o'} {
+		if base32encoding.ValidByte(b) {
+			t.Fatalf("%q should be invalid", b)
+		}
+	}
+	if base32encoding.ValidByte(0xff) {
+		t.Fatal("0xff should be invalid")
+	}
 }
